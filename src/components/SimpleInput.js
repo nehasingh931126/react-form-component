698adb601df679ec35f2fe65d9f5888c523a0020
Neha Singh <neha.singh931126@gmail.com>
2023-03-11 22:25:57 +0530

    added simple validation and message

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,24 +2,33 @@ import {useRef, useState} from 'react';
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
-  const nameChangeFieldHandler = (event)=> {
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const nameChangeFieldHandler = (event) => {
+    setEnteredNameIsValid(false);
     setEnteredName(event.target.value);
   }
+
   const formSubmissionHandler = (event)=> {
     event.preventDefault(); //Why because the HTTP Server request is sent so to avoid the default behaviour
     console.log(enteredName);
+    if(enteredName.trim() === '') { // validations
+      setEnteredNameIsValid(false);
+      return;
+    }
 
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue)
   }
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
   return (
-    <form>
-      <div className='form-control' onSubmit={formSubmissionHandler}>
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameChangeFieldHandler}/>
+        <input ref={nameInputRef} type='text' id='name' value={enteredName} onChange={nameChangeFieldHandler}/>
+        {!enteredNameIsValid  && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
-        <button type="submit">Submit</button>
+        <button>Submit</button>
       </div>
     </form>
   );
