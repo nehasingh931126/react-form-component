13b79ce972f5a7f520ec82a616e3d75b5f33081b
Neha Singh <neha.singh931126@gmail.com>
2023-03-11 22:00:25 +0530

    form handling using the useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,19 +1,23 @@
 import {useRef, useState} from 'react';
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
   const nameChangeFieldHandler = (event)=> {
     setEnteredName(event.target.value);
   }
   const formSubmissionHandler = (event)=> {
+    
     event.preventDefault(); //Why because the HTTP Server request is sent so to avoid the default behaviour
     console.log(enteredName);
-    // if(enteredName)
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue)
   }
   return (
     <form>
       <div className='form-control' onSubmit={formSubmissionHandler}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameChangeFieldHandler}/>
+        <input ref={nameInputRef} type='text' id='name' onChange={nameChangeFieldHandler}/>
       </div>
       <div className="form-actions">
         <button type="submit">Submit</button>
